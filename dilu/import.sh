1ab22cc784df9c85de1c4399d55261ad47180908
Ubuntu <viktorvector@gmail.com>
2017-01-02 19:03:47 +0000

    update the imported Django versions

--- dilu/import.sh
+++ dilu/import.sh
@@ -4,7 +4,7 @@ gitroot=$(git rev-parse --show-toplevel)
 test -d $gitroot/dilu/repo_django || git clone https://github.com/django/django.git $gitroot/dilu/repo_django
 git -C $gitroot/dilu/repo_django fetch
 
-for i in "1.9.2" "1.8.9" "1.7.2" "1.6.5"
+for i in "1.10.4" "1.9.12" "1.8.17"
 do
     python -B dump_version.py $i $i
     python -B load_version.py
